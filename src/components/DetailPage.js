2a585a2a65c044405f0ccaf7f42d5473e9d667fb
izorawar <sin11529@sheridancollege.ca>
2023-03-26 15:40:43 -0400

    first commit

---
+++ src/components/DetailPage.js
@@ -0,0 +1,32 @@
+import Modal from "react-bootstrap/Modal";
+import CloseButton from "react-bootstrap/CloseButton";
+import React, { useState } from 'react';
+
+export const DetailPage = ({ data, show, onClose, sinceTime, tillTime }) => {
+  return (
+    <>
+      <Modal show={show} onHide={onClose} centered>
+        <Modal.Header>
+          <Modal.Title>{data.title}</Modal.Title>
+          <CloseButton variant="white" onClick={onClose} />
+        </Modal.Header>
+        <Modal.Body>
+          <img src={data.image} className="mw-100" />
+          <br />
+          <br />
+          <p>
+            {sinceTime} - {tillTime}
+          </p>
+          <h4>Description</h4>
+          <p>{data.description}</p>
+          <p>
+            <b>Rating:</b> {data.rating}
+          </p>
+          <p>
+            <b>Genre:</b> {data.Genre}
+          </p>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
---
+++ src/components/ProgramItem.js
@@ -0,0 +1,77 @@
+import {
+  ProgramBox,
+  ProgramContent,
+  ProgramFlex,
+  ProgramStack,
+  ProgramTitle,
+  ProgramText,
+  ProgramImage,
+  useProgram
+} from "planby";
+import { DetailPage } from "./DetailPage";
+import React, { useState, useEffect } from 'react';
+
+export const ProgramItem = ({ program, ...rest }) => {
+  const {
+    styles,
+    formatTime,
+    set12HoursTimeFormat,
+    isLive,
+    isMinWidth
+  } = useProgram({
+    program,
+    ...rest
+  });
+
+  const { data } = program;
+  const { image, title, since, till } = data;
+
+  const formatTimeCustom = (time) => {
+    const date = new Date(time);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+
+    hours %= 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    return `${hours.toString().padStart(2, '0')}:${minutes}`;
+  };
+
+  const sinceTime = formatTimeCustom(since);
+  const tillTime = formatTimeCustom(till);
+  const [open, setOpen] = useState(false);
+
+  const handleModalClick = (e) => {
+    e.stopPropagation();
+  };
+
+  return (
+    <ProgramBox width={styles.width} style={styles.position} onClick={() => setOpen(true)}>
+      {open && (
+        <div onClick={handleModalClick}>
+          <DetailPage
+            data={data}
+            sinceTime={sinceTime}
+            show={open}
+            onClose={() => {
+              setOpen(false);
+            }}
+            tillTime={tillTime}
+          />
+        </div>
+      )}
+      <ProgramContent width={styles.width} isLive={isLive}>
+        <ProgramFlex>
+          {isLive && isMinWidth && <ProgramImage src={image} alt="Preview" />}
+          <ProgramStack>
+            <ProgramTitle>{title}</ProgramTitle>
+            <ProgramText>
+              {sinceTime} - {tillTime}
+            </ProgramText>
+          </ProgramStack>
+        </ProgramFlex>
+      </ProgramContent>
+    </ProgramBox>
+  );
+};
